b099fc40a7a23746161aba00f19aa93882de236d
Swastik <swastiK23>
2020-11-25 13:45:58 +0530

    Adding build-tools and Pinot src code

---
+++ build-tools/script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SRC_DIR="/mnt/d/Academics/SoftwareEvolution/pinot/src"
+AST_DIR="/mnt/d/Academics/SoftwareEvolution/pinot/src/ast_dump"
+CPA_FILENAME="Pinotv1.cpa"
+file_ext="cpp"
+WORD_LIST="ast.cpp"
+
+for entry in "$SRC_DIR"/*.$file_ext
+do
+	filename=$(echo "$entry" | rev | cut -d '/' -f 1| rev)
+	
+	
+        if echo $WORD_LIST | grep -w "$filename" > /dev/null; then
+          echo "Ignored :$filename....."
+        else
+          echo "processing :$filename....."
+	  clang++ -c -fno-delayed-template-parsing -fno-color-diagnostics -Xclang -ast-dump "$entry" > "$AST_DIR/$filename.ast" 
+        fi       
+done
+
+java -jar structure101-parser-ast-cpa.jar generate-cpa -use-db -gzip-compress -keep-going -ignore-compilation-errors -d "$AST_DIR/" -o "cpa_out/$CPA_FILENAME"
