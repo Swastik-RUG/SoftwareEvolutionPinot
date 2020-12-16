c8e3ff90dba653453b19d2fc458ad0e917c176b4
Swastik <swastiK23>
2020-12-16 21:32:55 +0530

    Adding Control.h refactoring

--- pinot/build/bin/run_pinot.sh
+++ pinot/build/bin/run_pinot.sh
@@ -1,4 +1,4 @@
 projectpath="/mnt/d/Academics/pinot/build/bin/designPatterns"
 projectname="designPatterns"
 find ${projectpath} -name '*.java' > ${projectname}-files.list
-pinot @${projectname}-files.list
+/mnt/d/Academics/SEaM/SoftwareEvolutionPinot/pinot/build/bin/pinot @${projectname}-files.list
---
+++ pinot/src/patterns/FactoryAnalysis.h
@@ -0,0 +1,44 @@
+#ifndef FactoryAnalysis_NAME_H
+#define FactoryAnalysis_NAME_H
+
+#include "platform.h"
+#include "symbol.h"
+#include "tuple.h"
+#include <vector>
+
+#ifdef HAVE_JIKES_NAMESPACE
+namespace Jikes
+{ // Open namespace Jikes block
+#endif
+
+    class Flatten;
+    class StoragePool;
+    // template<typename _Tp, typename _Alloc = std::allocator<_Tp> >
+    // class vector: protected _Vector_base<_Tp, _Alloc>;
+
+    class FactoryAnalysis
+    {
+    private:
+        MethodSymbol *method;
+        Flatten flatten;
+        static SymbolSet visited; // visited methods, avoid analysis on recursion methods.
+        StoragePool *ast_pool;
+
+    public:
+        static SymbolSet types;
+        FactoryAnalysis(MethodSymbol *msym, StoragePool *pool) : method(msym), ast_pool(pool) { flatten.init(msym, pool); }
+        bool IsFactoryMethod();
+        bool IsCreationMethod();
+        void CleanUp()
+        {
+            types.SetEmpty();
+            visited.SetEmpty();
+        }
+        ~FactoryAnalysis() {}
+    };
+
+#ifdef HAVE_JIKES_NAMESPACE
+} // Close namespace Jikes block
+#endif
+
+#endif
