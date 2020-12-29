369b616c5f2149a6ec5f84f3aa71bbdad55396c4
Sytse Oegema <sy.oegema@oegematransport.nl>
2020-12-29 15:14:24 +0100

    removed binary files

--- pinot/build/bin/run_pinot.sh
+++
@@ -1,4 +0,0 @@
-projectpath="/mnt/d/Academics/pinot/build/bin/designPatterns"
-projectname="designPatterns"
-find ${projectpath} -name '*.java' > ${projectname}-files.list
-/mnt/d/Academics/SEaM/SoftwareEvolutionPinot/pinot/build/bin/pinot @${projectname}-files.list
